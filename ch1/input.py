0d9eefe3739afff1fab38321424073dc311b9581
hacker59 <147670523+rashmindudhatra001@users.noreply.github.com>
2025-11-04 19:02:35 +0530

    Delete ch1 directory

--- ch1/input.py
+++
@@ -1,5 +0,0 @@
-name = input ("Enter your name :")
-
-age = int(input ("enter your age :"))
-
-print ("Hello " + name + " you are " + str(age) + " years old")
--- ch1/sum.py
+++
@@ -1,5 +0,0 @@
-x = int(input("Enter your fist number:"))
-y = int(input("Enter your second number:"))
-
-sum = x + y
-print("The sum of", x, "and", y, "is", sum)
